1ba52afc68ead5d88474a39171301c71d378f3ec
cirinatorres <torrescirina@yahoo.com>
2017-07-23 20:17:38 +0200

    Finished Normalization. End of Week 2

--- Week 2/machine-learning-ex1/ex1/ex1_multi.m
+++ Week 2/machine-learning-ex1/ex1/ex1_multi.m
@@ -166,9 +166,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
-
-
+H = [1, 1650, 3];
+price = H * theta;
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
