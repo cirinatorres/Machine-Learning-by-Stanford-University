d25c83e236b4abe3599a23702296e8f6ea9eb86d
cirinatorres <torrescirina@yahoo.com>
2017-07-29 21:32:46 +0200

    Regularized Logistic Regression Cost & Gradient

--- Week 3/machine-learning-ex2/ex2/costFunctionReg.m
+++ Week 3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -1,27 +1,26 @@
 function [J, grad] = costFunctionReg(theta, X, y, lambda)
-%COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
-%   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
-%   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+	% COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
+	% J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
+	% theta as the parameter for regularized logistic regression and the
+	% gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+	% Initialize some useful values
+	m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+	% You need to return the following variables correctly 
+	grad = zeros(size(theta));
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
+	% ====================== YOUR CODE HERE ======================
+	% Instructions: Compute the cost of a particular choice of theta.
+	%               You should set J to the cost.
+	%               Compute the partial derivatives and set grad to the partial
+	%               derivatives of the cost w.r.t. each parameter in theta
+	J = (1/m)*sum(-y'*log(sigmoid(X*theta)) - (1-y')*log(1-sigmoid(X*theta))) + (lambda/(2*m))*sum(theta(2:end,:).^2);
 
-
-
-
-
-
-% =============================================================
-
-end
+	grad_aux = grad;
+	grad_aux(1) = (1/m)*sum(sigmoid(X*theta)-y)*X(1,1);
+	for j = 2:size(theta)
+		grad_aux(j) = (1/m)*sum((sigmoid(X*theta)-y).*X(:,j)) + lambda*theta(j)/m;
+	grad = grad_aux;
+	% =============================================================
+end;
